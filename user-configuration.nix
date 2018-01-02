9e31887d4e32f122a6ad301b670e8e0fbb51bafd
Jacob Jonsson <jassob@dtek.se>
2018-01-02 14:29:16 +0100

    Added my nixos conf

---
+++ user-configuration.nix
@@ -0,0 +1,60 @@
+{ config, lib, pkgs, ... }:
+
+{
+# Define user accounts. This is the only way to add users to the
+# system since mutableUsers are false.
+  users = {
+    mutableUsers = false;
+
+    users.jassob = {
+      home = "/home/jassob";
+      description = "Jacob Jonsson";
+      extraGroups = [
+        "wheel"
+        "disk"
+        "audio"
+        "video"
+        "systemd-journal"
+        "sudo"
+        "users"
+        "networkmanager"
+        "docker"
+      ];
+      createHome = true;
+      uid = 1000;
+      shell = pkgs.zsh;
+      hashedPassword = "CREATE WITH mkpasswd -m sha-512";
+      packages = with pkgs;
+      [
+        # Applications
+        conkeror
+        drive
+        dropbox-cli
+        firefox
+        keybase
+        pass
+        pavucontrol
+        steam
+        stow
+        termite
+        virtmanager
+
+        # Haskell development
+        haskellPackages.ghc
+        haskellPackages.hlint
+        haskellPackages.cabal-install
+        haskellPackages.cabal2nix
+
+        # For my XMonad setup
+        compton
+        dunst
+        imagemagick
+        libnotify
+        rofi
+        scrot
+        sxhkd
+        xorg.xbacklight
+     ];
+    };
+  };
+}
