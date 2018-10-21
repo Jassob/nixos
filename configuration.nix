3ae0762e469b2db726dda9df45f8729866bda104
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2018-10-21 14:29:30 +0200

    Add upower service

--- configuration.nix
+++ configuration.nix
@@ -93,6 +93,9 @@
     powertop.enable = true;
   };
 
+  services.upower.enable = true;
+  systemd.services.upower.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.bash = {
