ceb88003607f6adb33651f1225b2ecf8f9028b80
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2018-10-21 14:28:59 +0200

    Bump NixOS: 17.09 -> 18.03

--- configuration.nix
+++ configuration.nix
@@ -213,9 +213,9 @@
     ];
   };
 
-  # This value determines the NixOS release with which your system is
-  # to be compatible, in order to avoid breaking some software such as
-  # database servers. You should change this only after NixOS release
-  # notes say you should.
-  system.stateVersion = "17.09"; # Did you read the comment?
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.03"; # Did you read the comment?
 }
