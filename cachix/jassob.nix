88778766f2cd55bb4599580407af9dc83668065f
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2023-01-14 15:04:14 +0100

    initial commit
    Commit the current state of the configuration.

---
+++ cachix/jassob.nix
@@ -0,0 +1,11 @@
+
+{
+  nix.settings = {
+    substituters = [
+      "https://jassob.cachix.org"
+    ];
+    trusted-public-keys = [
+      "jassob.cachix.org-1:wl+u5/b5NqlQoG70EXHctvD3oNcosaVrEWltjKXPROU="
+    ];
+  };
+}
