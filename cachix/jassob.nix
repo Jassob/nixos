8d9b1c32bec2fc44251f1a62a1d6ba55eb0ad397
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2025-01-18 15:47:31 +0100

    chore: add new jassob.cachix.org public key

--- cachix/jassob.nix
+++ cachix/jassob.nix
@@ -6,6 +6,7 @@
     ];
     trusted-public-keys = [
       "jassob.cachix.org-1:wl+u5/b5NqlQoG70EXHctvD3oNcosaVrEWltjKXPROU="
+      "jassob.cachix.org-1:XlVII2RxIu5l6uoenxM1+Y1e4iKJQ8tKp+TSgfTDpww="
     ];
   };
 }
