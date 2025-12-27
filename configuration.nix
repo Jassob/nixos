326e2594079d97df8259a7dbbeb7ed2351dbf9c0
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2025-12-27 21:03:53 +0100

    refactor: move more stuff into desktop.nix

--- configuration.nix
+++ configuration.nix
@@ -38,20 +38,6 @@
     usbutils
   ];
 
-  fonts.packages = with pkgs; [
-    corefonts # Microsoft free fonts
-    dejavu_fonts
-    hasklig
-    inconsolata
-    input-fonts
-    iosevka
-    ubuntu-classic
-    xits-math
-    nerd-fonts.iosevka
-    nerd-fonts.fira-code
-    nerd-fonts.inconsolata
-  ];
-
   hardware = {
     keyboard.zsa.enable = true;
 
